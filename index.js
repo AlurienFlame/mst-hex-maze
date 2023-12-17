24b814ee516499a71355322b92a50272188be257
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2023-12-17 11:45:00 -0800

    Outer edge rendering

--- index.js
+++ index.js
@@ -21,44 +21,90 @@ class HexGrid {
         return newNode;
     }
 
+    addEdge(a, b) {
+        let newEdge = { a, b, open: false };
+        this.edges.push(newEdge);
+
+        //  Add edge to nodes
+        a.edges = a.edges || [];
+        a.edges.push(newEdge);
+        b.edges = b.edges || [];
+        b.edges.push(newEdge);
+    }
+
     render() {
-        // Draw hexes
-        // for (let node of Object.values(this.nodes)) {
-        //     let { x, y } = cubicToPixel(node.q, node.r);
-        //     drawHexagon(x, y);
+        // Paths based on edges
+        // ctx.strokeStyle = '#00ff00';
+        // for (let edge of this.edges.filter(edge => edge.open)) {
+        //     let pointA = cubicToPixel(edge.a.q, edge.a.r);
+        //     let pointB = cubicToPixel(edge.b.q, edge.b.r);
+        //     ctx.beginPath();
+        //     ctx.moveTo(pointA.x, pointA.y);
+        //     ctx.lineTo(pointB.x, pointB.y);
+        //     ctx.stroke();
+        //     ctx.closePath();
         // }
 
-        // Edges
-        for (let edge of this.edges) {
+        // Inner borders
+        ctx.strokeStyle = '#000000';
+        for (let edge of this.edges.filter(edge => !edge.open)) {
             let pointA = cubicToPixel(edge.a.q, edge.a.r);
             let pointB = cubicToPixel(edge.b.q, edge.b.r);
-            ctx.beginPath();
-            // ctx.moveTo(pointA.x, pointA.y);
-            // ctx.lineTo(pointB.x, pointB.y);
-            // ctx.strokeStyle = '#ff0000';
-            // ctx.stroke();
             let halfway = {
                 x: (pointA.x + pointB.x) / 2,
                 y: (pointA.y + pointB.y) / 2
             };
+            ctx.beginPath();
             ctx.translate(halfway.x, halfway.y);
             ctx.rotate(Math.atan2(pointB.y - pointA.y, pointB.x - pointA.x));
-            ctx.moveTo(0, 0);
+            ctx.moveTo(0, -scale / 2);
             ctx.lineTo(0, scale / 2);
-            ctx.strokeStyle = '#ff0000';
             ctx.stroke();
             ctx.rotate(-Math.atan2(pointB.y - pointA.y, pointB.x - pointA.x));
             ctx.translate(-halfway.x, -halfway.y);
             ctx.closePath();
         }
 
-        // Coordinates
-        for (let node of Object.values(this.nodes)) {
-            let { x, y } = cubicToPixel(node.q, node.r);
-            ctx.font = '10px Arial';
-            ctx.fillStyle = '#000000';
-            ctx.fillText(`${node.q},${node.r},${node.s}`, x, y);
+        // Outer border
+        for (let node of Object.values(this.nodes).filter(node => node.edges.length < 6)) {
+            let directions = [
+                { q: 1, r: -1 },
+                { q: 1, r: 0 },
+                { q: 0, r: -1 },
+                { q: 0, r: 0 },
+                { q: 0, r: 1 },
+                { q: -1, r: 0 },
+                { q: -1, r: 1 },
+            ];
+            for (let direction of directions) {
+                let neighbor = this.findNode(node.q + direction.q, node.r + direction.r, -(node.q + direction.q) - (node.r + direction.r));
+                if (!neighbor) {
+                    let pointA = cubicToPixel(node.q, node.r);
+                    let pointB = cubicToPixel(node.q + direction.q, node.r + direction.r);
+                    let halfway = {
+                        x: (pointA.x + pointB.x) / 2,
+                        y: (pointA.y + pointB.y) / 2
+                    };
+                    ctx.beginPath();
+                    ctx.translate(halfway.x, halfway.y);
+                    ctx.rotate(Math.atan2(pointB.y - pointA.y, pointB.x - pointA.x));
+                    ctx.moveTo(0, -scale / 2);
+                    ctx.lineTo(0, scale / 2);
+                    ctx.stroke();
+                    ctx.rotate(-Math.atan2(pointB.y - pointA.y, pointB.x - pointA.x));
+                    ctx.translate(-halfway.x, -halfway.y);
+                    ctx.closePath();
+                }
+            }
         }
+
+        // Coordinates
+        // for (let node of Object.values(this.nodes)) {
+        //     let { x, y } = cubicToPixel(node.q, node.r);
+        //     ctx.font = '10px Arial';
+        //     ctx.fillStyle = '#000000';
+        //     ctx.fillText(`${node.q},${node.r},${node.s}`, x, y);
+        // }
     }
 
     generateSevenHexes(q, r) {
@@ -97,11 +143,13 @@ class HexGrid {
     }
 
     createEdgesBetween(nodes) {
-        for (let a of Object.values(nodes)) {
-            for (let b of Object.values(nodes)) {
+        for (let i = 0; i < nodes.length; i++) {
+            for (let j = i + 1; j < nodes.length; j++) {
+                let a = nodes[i];
+                let b = nodes[j];
                 if (a === b) continue;
                 if (cubicDistance(a, b) > 1) continue;
-                this.edges.push({ a, b });
+                this.addEdge(a, b);
             }
         }
     }
@@ -124,31 +172,46 @@ function cubicToPixel(q, r) {
     return { x, y };
 }
 
-function drawHexagon(x, y) {
-    ctx.translate(x, y);
-    ctx.beginPath();
-
-    ctx.rotate(Math.PI / 6);
-    ctx.moveTo(scale, 0);
-    for (let i = 0; i < 6; i++) {
-        ctx.rotate(Math.PI / 3);
-        ctx.lineTo(scale, 0);
-    }
-    ctx.rotate((2 * Math.PI) - (Math.PI / 6)); // Reset rotation
-
-    ctx.strokeStyle = '#000000';
-    ctx.stroke();
-    ctx.closePath();
-    ctx.translate(-x, -y);
-}
-
 function generateHexMaze(nodes, edges) {
     // Abstract the grid to a graph and apply prim's
 
-    // TODO
+    for (let edge of edges) {
+        edge.weight = Math.random();
+    }
+
+    let visited = [nodes[0]];
+    let spanningTree = [];
+
+    while (visited.length < nodes.length) {
+        // Find all edges that are connected to the visited nodes
+        let frontier = visited.reduce((frontier, node) => {
+            return frontier.concat(node.edges);
+        }, []);
+        frontier = frontier.filter(edge => {
+            return !visited.includes(edge.a) || !visited.includes(edge.b);
+        });
+
+        // Find the smallest of those nodes
+        let minEdge = frontier.reduce((min, edge) => {
+            return edge.weight < min.weight ? edge : min;
+        }, { weight: Infinity });
+
+        // Add it to the spanning tree
+        spanningTree.push(minEdge);
+
+        // Mark newly connected nodes as visited
+        if (!visited.includes(minEdge.a))
+            visited.push(minEdge.a);
+        if (!visited.includes(minEdge.b))
+            visited.push(minEdge.b);
+    }
+
+    for (let edge of spanningTree) {
+        edge.open = true;
+    }
 }
 
 const graph = new HexGrid();
 graph.generateFourtyNineHexes(2, 5);
+generateHexMaze(Object.values(graph.nodes), graph.edges);
 graph.render();
-generateHexMaze(graph.nodes, graph.edges);
